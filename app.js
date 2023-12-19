f44a386280e5aaf97ced7f57b51203d69f35927c
Olga <olga_lesechko@mail.ru>
2023-12-19 18:10:51 +0500

    first

---
+++ app.js
@@ -0,0 +1,34 @@
+//var imya = 'Olga';
+
+
+//console.log('Я ' + imya);
+
+var imya = prompt('Введите Ваше имя');
+console.log('Меня зовут ' + imya);
+
+
+var age = prompt('Введите Ваш возраст');
+console.log('Мне ' + age + ' года/лет');
+
+var sum = prompt('Решите пример 5 + 3 = ?');
+console.log('Пример 1: 5 + 3 = ' + (5 + 3) + '. Ваш ответ ' + sum);
+
+var minus = prompt('Решите пример 6 - 2 = ?');
+console.log('Пример 2: 6 - 2 = ' + (6 - 2) + '. Ваш ответ ' + minus);
+
+var multi = prompt('Решите пример 4 * 9 = ?');
+console.log('Пример 3: 4 * 9 = ' + 4 * 9 + '. Ваш ответ ' + multi);
+
+var del = prompt('Решите пример 9 / 3 = ?');
+console.log('Пример 4: 9 / 3 = ' + 9 / 3 + '. Ваш ответ ' + del);
+
+var deg = prompt('Решите пример 10 % 4 = ?');
+console.log('Пример 5: 10 % 4 = ' + 10 % 4 + '. Ваш ответ ' + deg);
+
+alert('Откройте консоль для просмотра')
+
+var one = +prompt('Введите первое число');
+var two = +prompt('Введите второе число');
+var three = +prompt('Введите третье число');
+
+alert('Среднее арифметическое: ' + (one + two+three)/3);
